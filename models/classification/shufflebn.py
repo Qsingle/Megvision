3f1d982f5a6ffe87354a1a9d65435fb5508a2b62
Qsingle <1271808136@qq.com>
2021-05-08 14:32:42 +0800

    remove shufflebn from classsification

--- models/classification/shufflebn.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# !/usr/bin/env python
-"""
-    @author: Zhongxi Qiu
-    @create time: 2021/4/20 9:59
-    @filename: shufflebn.py
-    @software: PyCharm
-"""
-from megengine.autodiff import Function

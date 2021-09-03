d9beee1c8bbf8ac8098b93d231b2250a65d0d30a
Qsingle <1271808136@qq.com>
2021-09-03 16:18:20 +0800

    update

--- megvision/comm/__init__.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
-"""
-@File    :  __init__.py.py
-@Time    :  2021/8/26 16:17
-@Author  :  Zhongxi Qiu
-@Contact :  qiuzhongxi@163.com
-@License :  (C)Copyright 2019-2021
-"""
